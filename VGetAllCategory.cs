186a861ae53362cb5398e8cfd7e7fc71403cfd67
Fernando Beira <fernando.beira@hsl.org.br>
2024-02-05 21:23:08 -0300

    first commit

---
+++ VGetAllCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDatabase004;
+
+public partial class VGetAllCategory
+{
+    public string ParentProductCategoryName { get; set; } = null!;
+
+    public string? ProductCategoryName { get; set; }
+
+    public int? ProductCategoryId { get; set; }
+}
