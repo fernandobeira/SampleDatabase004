f4b28aa909d91de7dc8e4ad660fe7b84de7f1359
Fernando Beira <fernando.beira@hsl.org.br>
2024-02-19 18:14:04 -0300

    Project Publishing

--- Program.cs
+++ Program.cs
@@ -22,11 +22,11 @@ builder.Services.AddDbContext<Db2netr004Context>(option => option.UseSqlServer(c
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
